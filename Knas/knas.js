42b6d2e4ad7130af1e3f9a522e60f64606efd5b4
Blacky145 <46751919+Blacky145@users.noreply.github.com>
2019-09-13 13:39:02 +0200

    Update knas.js

--- Knas/knas.js
+++ Knas/knas.js
@@ -1,6 +1,6 @@
-let csvToJson = require('convert-csv-to-json');
+let csvToJson = require('convert-csv-to-json'); //Läser modulen
 
-let fileInputName = 'myInputFile.csv';
-let fileOutputName = 'myOutputFile.json';
+let fileInputName = 'myInputFile.csv'; //fileInputName blir variabel som då är = min csv kod.
+let fileOutputName = 'myOutputFile.json'; //fileOutputName blir variabel till min blivande json kod.
 
-csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
+csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName); //Denna konverterar csv filen till json.
